833436e32d1d4826cb1c7aa1fae8346ae16c6e16
Raúl Crespo Díaz <36744556+RaulCrespoDiaz@users.noreply.github.com>
2018-06-13 16:13:37 +0200

    Add files via upload

---
+++ Animated Version/main.js
@@ -0,0 +1,140 @@
+var format = d3.format(",");
+
+// Set tooltips
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function (d) {
+    return "<strong>Country: </strong><span class='details'>"
+      + d.properties.name
+      + "<br></span>"
+      + "<strong>Population: </strong><span class='details'>"
+      + format(d.population)
+      + "</span>";
+  })
+
+// Setting camvas size
+var margin = { top: 0, right: 0, bottom: 0, left: 0 },
+  width = 960 - margin.left - margin.right,
+  height = 700 - margin.top - margin.bottom;
+
+// the color of the map with its scale
+var color = d3.scaleThreshold()
+  .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
+  .range([
+    "#FEFCED",
+    "#FFF8D9",
+    "#FEF5BD",
+    "#FFEDB8",
+    "#FDE3B3",
+    "#F8CFAA",
+    "#F1A893",
+    "#E47479",
+    "#C03F58",
+    "#760420"
+  ]);
+
+var path = d3.geoPath();
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append('g')
+  .attr('class', 'map');
+
+var projection = d3.geoMercator()
+  .scale(130)
+  .translate([width / 2, height / 1.5]);
+
+var path = d3.geoPath().projection(projection);
+
+svg.call(tip);
+
+
+var inityear = 0;
+var gpopulation;
+var gdata;
+
+queue()
+  .defer(d3.json, "world_countries.json")
+  .defer(d3.json, "country-full-population.json")
+  .await(ready);
+
+function ready(error, data, population) {
+  gdata = data;
+  gpopulation = population;
+  drawchart();
+  setInterval(drawchart, 1000)
+}
+
+
+function drawchart() {
+
+  var populationById = {};
+  var paises = [];
+
+  //select the max year
+  var maxyear = d3.max(gpopulation, function(d, i) {
+    return d.Year;
+  });
+
+  //select the min year
+  var minyear = d3.min(gpopulation, function(d, i) {
+    return d.Year;
+  });
+
+  // we have to choose the right year when we are iterating
+  if (inityear===0)
+    inityear=minyear;
+  else if (inityear===maxyear)
+    inityear=minyear;
+  else
+    {    
+      inityear++;
+    }
+  year=inityear;
+    
+  document.getElementById("mitexto").innerHTML = year.toString();
+
+  //preparing and filtering the data we want to show
+  var paises = gpopulation.filter(gdata => gdata.Year === year)
+  .map(gdata => ([gdata["Country Code"], gdata.Value]));
+  paises.forEach(function (d) { populationById[d[0]] = +d[1]; });
+ 
+  gdata.features.forEach(function (d) { d.population = populationById[d.id] });
+
+  //Adding the svg map with its colours
+  svg.append("g")
+    .attr("class", "countries")
+    .selectAll("path")
+    .data(gdata.features)
+    .enter().append("path")
+    .attr("d", path)
+    .style("fill", function (d) { return color(populationById[d.id]); })
+    .style('stroke', 'white')
+    .style('stroke-width', 1.5)
+    .style("opacity", 0.8)
+    // tooltips
+    .style("stroke", "white")
+    .style('stroke-width', 0.3)
+    .on('mouseover', function (d) {
+      tip.show(d);
+
+      d3.select(this)
+        .style("opacity", 1)
+        .style("stroke", "white")
+        .style("stroke-width", 3);
+    })
+    .on('mouseout', function (d) {
+      tip.hide(d);
+
+      d3.select(this)
+        .style("opacity", 0.8)
+        .style("stroke", "white")
+        .style("stroke-width", 0.3);
+    })
+    ;
+}
+
+
